b8674242c8f51adf6c430859b11b4f74c5926778
Malin W <malin.wiren@hotmail.com>
2025-05-31 13:18:46 +0200

    beginning of streamlit application, reads file

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
+import pandas as pd
+import requests
+from io import StringIO
 import streamlit as st
 
-st.write("# Hello World")
+@st.cache_data
+def load_data():
+    url = "https://raw.githubusercontent.com/Livorya/python-assignment/refs/heads/main/data/diamonds.csv?token=GHSAT0AAAAAADEGUNIDTVQUU2CPFXKZGST62B24R2A"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return pd.read_csv(StringIO(response.text))
+    else:
+        st.error("Failed to load data from GitHub.")
+        return None
+    
+
+st.write("# Diamonds!!")
+
+df = load_data()
+st.write("###### Table of Diamonds.csv")
+st.dataframe(df)
 
